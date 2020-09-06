d575762ba72ed879c434ab20fb8defb0413ca5b7
MisterSherlockHolmes <48397884+MisterSherlockHolmes@users.noreply.github.com>
2020-09-06 08:41:21 +0800

    Add files via upload

---
+++ 危险的病毒.bat
@@ -0,0 +1,8 @@
+title 黑客帝国
+setlocal ENABLEDELAYEDEXPANSION
+msg /server:192.168.3.25 * 我黑进来了，哈哈哈哈哈，你电脑玩完了
+msg /server:192.168.3.25 * 逗你玩的，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈或或或或
+msg /server:192.168.3.25 * 不对，我又反悔了，几秒种后你的电脑会发生一些事情
+msg /server:192.168.3.25 * 即将开始播放上世纪80年代大片：星球大战
+telnet towel.blinkenlights.nl
+
